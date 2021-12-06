1b39c70b2bc99bff3e34eb1596fc53d4c23f0907
Candice Smith MacKenzie <Mahony8@aol.com>
2021-12-05 22:30:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+//   function titleCased(str){
+//     str.split(' ').map(w => w[0].toUpperCase()) + w.substr(1).toLowerCase().join(' ')
+//  }
+//  titleCased(tutorials);
+//function titleCased(str){
+//  return str.charAt(0).toUpperCase()+ str.slice(1);
+//}
+//titleCased(tutorials)
+
+//const capTutorials = str.split(' ').map(titleCased).join(' ');
+//capTutorials;
+
+// function titleCased(str) {
+//   let splitStr= str.toLowerCase().split(' ');
+//   for (let i = 0; i < splitStr.length; i++) {
+//      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+//   }
+//   return splitStr.join(' ');
+// }
+
+// const words = tutorials.split(" ");
+
+// words.map((word) => { 
+//     return word[0].toUpperCase() + word.substring(1); 
+// }).join(" ");
+const titleCased = function() {
+  return tutorials.map((title) => {
+    return title.split(' ').map((word) => {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+  });
+};
+console.log(titleCased(tutorials))
